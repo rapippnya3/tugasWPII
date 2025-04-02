93117dea88ffe1c8f49f16c8849b3d3040bfb6b1
rapippnya3 <159519457+rapippnya3@users.noreply.github.com>
2025-04-02 20:59:22 +0700

    Add files via upload

---
+++ tugasWPII2/views/v_html/getlorem.blade.php
@@ -0,0 +1,20 @@
+<p>
+    <b>Mengenal Huruf Tebal</b>
+    <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+
+<p>
+    <i>Mengenal Huruf Miring</i>
+    <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+
+<p>
+    <u>Mengenal Huruf Garis Bawah</u>
+    <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
